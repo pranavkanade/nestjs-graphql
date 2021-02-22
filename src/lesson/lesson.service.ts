bdab2f7a5bcd0f1a5880667b71f58c1855b4d586
pskanade <k.pranav.apk@gmail.com>
2021-02-22 23:26:27 +0530

    added validation and created input type (dto)

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
 import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
 
 @Injectable()
 export class LessonService {
@@ -11,10 +12,11 @@ export class LessonService {
   ) {}
 
   async getLessonById(id: string): Promise<Lesson> {
-    return this.lessonRepository.findOne({ id });
+    return this.lessonRepository.findOneOrFail({ id });
   }
 
-  async createLesson(name, startDate, endDate): Promise<Lesson> {
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const { name, startDate, endDate } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
