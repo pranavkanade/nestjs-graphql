5009879abf54ec808c8a8587fe4ad0940d2ad493
pskanade <k.pranav.apk@gmail.com>
2021-02-23 00:28:37 +0530

    Can add students to lessons separately or on create

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -20,14 +20,25 @@ export class LessonService {
   }
 
   async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
-    const { name, startDate, endDate } = createLessonInput;
+    const { name, startDate, endDate, students } = createLessonInput;
     const lesson = this.lessonRepository.create({
       id: uuid(),
       name,
       startDate,
       endDate,
+      students,
     });
     // this saves in the db
+    return this.lessonRepository.save(lesson);
+  }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOneOrFail({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentIds];
+
     return this.lessonRepository.save(lesson);
   }
 }
